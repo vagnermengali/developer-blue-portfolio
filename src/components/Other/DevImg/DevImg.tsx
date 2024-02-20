c6e29180ac49719e67693b23c141c440846800b8
vagnermengali <vagmengali2015@gmail.com>
2024-02-20 16:28:39 -0300

    feat: add new qa

--- src/components/Other/DevImg/DevImg.tsx
+++ src/components/Other/DevImg/DevImg.tsx
@@ -5,7 +5,7 @@ import { ImageInterface } from "@/interfaces/ImageInterface";
 const DevImg = ({ containerStyles, containerStylesImage, imgSrc }: ImageInterface) => {
   return (
     <div className={`${containerStyles} overflow-hidden`}>
-      <Image className={`${containerStylesImage} overflow-hidden`} src={imgSrc} width={1000} height={1000} priority alt="Profile Image"></Image>
+      <Image className={`${containerStylesImage} overflow-hidden`} src={imgSrc} width={500} height={500} priority alt="Profile Image"></Image>
     </div>
   );
 };
