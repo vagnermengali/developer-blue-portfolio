61f02c63d5e33df016714db997d983e92f85fede
vagnermengali <vagmengali2015@gmail.com>
2024-02-21 16:10:56 -0300

    feat: add new qa

--- src/components/Other/Seo/index.tsx
+++ src/components/Other/Seo/index.tsx
@@ -1,18 +1,20 @@
 import Head from "next/head";
-
 import { SeoInterface } from "@/interfaces/SeoInterface";
 
 const Seo = ({ title, description }: SeoInterface) => {
-
     return (
         <Head>
             <meta name="author" content="Vagner Mengali" />
             <meta name="keywords" content="Javascript" />
-            <link rel="fluid-icon" href="icon/favicon.svg" title="Vagner Mengali" />
-            <link rel="icon" type="image/svg+xml" href="icon/favicon.svg" />
+            <link rel="fluid-icon" href="/favicon.webp" title="Vagner Mengali" />
+            <link rel="icon" type="svg+xml" href="/favicon.webp" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <title>{title === "Homepage" ? "Vagner Mengali • Portfólio" : `${title} • Vagner Mengali • Portfólio`}</title>
-            <link rel="shortcut icon" href="icon/favicon.svg" type="image/x-icon" />
+            <link rel="shortcut icon" href="/favicon.webp" type="x-icon" />
+            <link rel="icon" href="/favicon.webp" sizes="32x32" />
+            <link rel="icon" href="/favicon.webp" sizes="48x48" />
+            <link rel="icon" href="/favicon.webp" sizes="96x96" />
+            <link rel="icon" href="/favicon.webp" sizes="144x144" />
             <meta name="description" content={description} />
             <meta property="og:title" content={title} />
             <meta property="og:description" content={description} />
@@ -31,6 +33,11 @@ const Seo = ({ title, description }: SeoInterface) => {
             <meta property="al:android:app_name" content="Vagner Mengali" />
             <meta property="al:android:url" content="https://vagnermengali.com/" />
             <link rel="canonical" href="https://vagnermengali.com/" />
+            <meta name="theme-color" content="#090a1a" />
+            <meta name="msapplication-TileColor" content="#090a1a" />
+            <meta name="msapplication-navbutton-color" content="#090a1a" />
+            <meta name="apple-mobile-web-app-status-bar-style" content="#090a1a" />
+            <link rel="manifest" href={"/manifest.webmanifest"} />
         </Head>
     );
 };
