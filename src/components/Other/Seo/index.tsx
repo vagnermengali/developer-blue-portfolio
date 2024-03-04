83747208683038092131f4f57ea48f5f63a67997
vagnermengali <vagmengali2015@gmail.com>
2024-03-04 16:54:28 -0300

    feat: add new qa

--- src/components/Other/Seo/index.tsx
+++ src/components/Other/Seo/index.tsx
@@ -22,21 +22,21 @@ const Seo = ({ title, description }: SeoInterface) => {
             <meta property="og:type" content="website" key="ogtype" />
             <meta property="og:url" content="https://vagnermengali.com/" />
             <meta property="og:image:alt" content={description} />
-            <meta property="og:image" content="/banner.webp" />
+            <meta property="og:image" content="/portfolio-blue.webp" />
             <meta name="twitter:title" content="Vagner Mengali" />
             <meta name="twitter:site" content="@vagnermengali" />
             <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:image" content="/banner.webp" />
+            <meta name="twitter:image" content="/portfolio-blue.webp" />
             <meta name="twitter:description" content={description} />
             <meta property="al:ios:app_name" content="Vagner Mengali" />
             <meta property="al:ios:url" content="https://vagnermengali.com/" />
             <meta property="al:android:app_name" content="Vagner Mengali" />
             <meta property="al:android:url" content="https://vagnermengali.com/" />
             <link rel="canonical" href="https://vagnermengali.com/" />
-            <meta name="theme-color" content="#090a1a" />
-            <meta name="msapplication-TileColor" content="#090a1a" />
-            <meta name="msapplication-navbutton-color" content="#090a1a" />
-            <meta name="apple-mobile-web-app-status-bar-style" content="#090a1a" />
+            <meta name="theme-color" content="#08d665" />
+            <meta name="msapplication-TileColor" content="#08d665" />
+            <meta name="msapplication-navbutton-color" content="#08d665" />
+            <meta name="apple-mobile-web-app-status-bar-style" content="#08d665" />
             <link rel="manifest" href={"/manifest.webmanifest"} />
             <meta name="robots" content="index,follow" />
             <meta name="googlebot" content="index, follow" />
