821396e0a0946fd7faa8b0ecdca06d1109964371
Vagner Mengali <vagmengali2015@gmail.com>
2024-03-08 18:21:39 -0300

    Atualizar o Logo.tsx

--- src/components/Other/Logo/Logo.tsx
+++ src/components/Other/Logo/Logo.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 const Logo = () => {
 
-  return (
+  return ( 
     <Link href="/">
-      <p className="sm:text-3xl text-2xl font-bold text-primary tracking-[4px] relative before:text-white before:rotate-180 before:top-[24.5px] sm:before:top-[30px] before:right-[4.3px] before:absolute before:content-['.'] before:w-2 before:h-2 whitespace-nowrap">Vagner Mengali</p>
+      <p className="sm:text-3xl text-2xl font-bold text-primary tracking-[4px] relative whitespace-nowrap">Vagner Mengali</p>
     </Link>
   );
 };
