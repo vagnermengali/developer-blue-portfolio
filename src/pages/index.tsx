9783ff4c913fc55fda8f7b1067a262252e2a83ff
Vagner Mengali <vagmengali2015@gmail.com>
2024-05-08 16:30:02 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Home from "@/components/Templates/Home/Home"
 const HomePage = () => {
   return (
     <>
-      <Seo description="Descubra um ponto de partida dinâmico para explorar meu mundo de desenvolvimento de software, com insights sobre minhas habilidades, experiência e abordagem única para resolver desafios tecnológicos." title="Homepage"/>
+      <Seo description="Descubra um ponto de partida dinâmico para explorar meu mundo de desenvolvimento de software, com insights sobre minhas habilidades, experiência e abordagem única para resolver desafios tecnológicos." title="Vagner Mengali • Portfólio"/>
       <Home />
     </>
   )
