05c4e80589d7e42bae8114dc5adeceba3ce0dd0e
Vagner Mengali <vagmengali2015@gmail.com>
2024-05-08 16:30:18 -0300

    Update contact.tsx

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -4,7 +4,7 @@ import Contact from "@/components/Templates/Contact/Contact"
 const ContactPage = () => {
   return (
     <>
-      <Seo description="Pronto para iniciar uma conversa sobre seu próximo projeto? Entre em contato comigo aqui. Estou ansioso para discutir suas ideias e como posso ajudá-lo a alcançar seus objetivos de desenvolvimento de software." title="Contato"/>
+      <Seo description="Pronto para iniciar uma conversa sobre seu próximo projeto? Entre em contato comigo aqui. Estou ansioso para discutir suas ideias e como posso ajudá-lo a alcançar seus objetivos de desenvolvimento de software." title="Contato • Vagner Mengali • Portfólio"/>
       <Contact />
     </>
   )
