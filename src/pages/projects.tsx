966684b919d2258e8004dd90e024503c2f694fc7
Vagner Mengali <vagmengali2015@gmail.com>
2024-05-08 16:29:43 -0300

    Update projects.tsx

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -4,7 +4,7 @@ import Projects from "@/components/Templates/Projects/Projects"
 const ProjectsPage = () => {
   return (
     <>
-      <Seo description="Explore uma galeria de projetos inspiradores e inovadores. Cada projeto é uma demonstração do meu compromisso com a excelência técnica e a entrega de soluções sob medida para os clientes." title="Projetos"/>
+      <Seo description="Explore uma galeria de projetos inspiradores e inovadores. Cada projeto é uma demonstração do meu compromisso com a excelência técnica e a entrega de soluções sob medida para os clientes." title="Projetos • Vagner Mengali • Portfólio"/>
       <Projects />
     </>
   )
